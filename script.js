aa9f2419a4a32dbefe6da48d535e98f37a8f3913
Laidybird <predaveronica72@yahoo.com>
2022-12-06 17:17:00 +0000

    Adiing functionality to the game

--- script.js
+++ script.js
@@ -1,220 +1,172 @@
-//Initial References
-let draggableObjects;
-let dropPoints;
+const moves = document.getElementById("moves-count");
+const timeValue = document.getElementById("time");
 const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector(".game-container");
 const result = document.getElementById("result");
 const controls = document.querySelector(".controls-container");
-const dragContainer = document.querySelector(".draggable-objects");
-const dropContainer = document.querySelector(".drop-points");
-const data = [
-  "belgium",
-  "bhutan",
-  "brazil",
-  "china",
-  "cuba",
-  "ecuador",
-  "georgia",
-  "germany",
-  "hong-kong",
-  "india",
-  "iran",
-  "myanmar",
-  "norway",
-  "spain",
-  "sri-lanka",
-  "sweden",
-  "switzerland",
-  "united-states",
-  "uruguay",
-  "wales",
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+//Items array
+const items = [
+  { name: "antigua", image: "antigua.png" },
+  { name: "australia", image: "australia.png" },
+  { name: "brazil", image: "brazil.png" },
+  { name: "ethiopia", image: "ethiopia.png" },
+  { name: "france", image: "france.png" },
+  { name: "germany", image: "germany.png" },
+  { name: "romania", image: "romania.png" },
+  { name: "samoa", image: "samoa.png" },
+  { name: "sweden", image: "sweden.png" },
+  { name: "togo", image: "togo.png" },
+  { name: "united-kingdom", image: "united-kingdom.png" },
+  { name: "barbados", image: "barbados.png" },
 ];
-
-let deviceType = "";
-let initialX = 0,
-  initialY = 0;
-let currentElement = "";
-let moveElement = false;
-
-//Detect touch device
-const isTouchDevice = () => {
-  try {
-    //We try to create Touch Event (It would fail for desktops and throw error)
-    document.createEvent("TouchEvent");
-    deviceType = "touch";
-    return true;
-  } catch (e) {
-    deviceType = "mouse";
-    return false;
+//Initial Time
+let seconds = 0,
+  minutes = 0;
+//Initial moves and win count
+let movesCount = 0,
+  winCount = 0;
+//For timer
+const timeGenerator = () => {
+  seconds += 1;
+  //minutes logic
+  if (seconds >= 60) {
+    minutes += 1;
+    seconds = 0;
   }
+  //format time before displaying
+  let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+  let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+  timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
 };
-
-let count = 0;
-
-//Random value from Array
-const randomValueGenerator = () => {
-  return data[Math.floor(Math.random() * data.length)];
+//For calculating moves
+const movesCounter = () => {
+  movesCount += 1;
+  moves.innerHTML = `<span>Moves:</span>${movesCount}`;
 };
-
-//Win Game Display
-const stopGame = () => {
-  controls.classList.remove("hide");
-  startButton.classList.remove("hide");
+//Pick random objects from the items array
+const generateRandom = (size = 4) => {
+  //temporary array
+  let tempArray = [...items];
+  //initializes cardValues array
+  let cardValues = [];
+  //size should be double (4*4 matrix)/2 since pairs of objects would exist
+  size = (size * size) / 2;
+  //Random object selection
+  for (let i = 0; i < size; i++) {
+    const randomIndex = Math.floor(Math.random() * tempArray.length);
+    cardValues.push(tempArray[randomIndex]);
+    //once selected remove the object from temp array
+    tempArray.splice(randomIndex, 1);
+  }
+  return cardValues;
 };
-
-//Drag & Drop Functions
-function dragStart(e) {
-  if (isTouchDevice()) {
-    initialX = e.touches[0].clientX;
-    initialY = e.touches[0].clientY;
-    //Start movement for touch
-    moveElement = true;
-    currentElement = e.target;
-  } else {
-    //For non touch devices set data to be transfered
-    e.dataTransfer.setData("text", e.target.id);
+const matrixGenerator = (cardValues, size = 4) => {
+  gameContainer.innerHTML = "";
+  cardValues = [...cardValues, ...cardValues];
+  //simple shuffle
+  cardValues.sort(() => Math.random() - 0.5);
+  for (let i = 0; i < size * size; i++) {
+    /*
+        Create Cards
+        before => front side (contains question mark)
+        after => back side (contains actual image);
+        data-card-values is a custom attribute which stores the names of the cards to match later
+      */
+    gameContainer.innerHTML += `
+     <div class="card-container" data-card-value="${cardValues[i].name}">
+        <div class="card-before">?</div>
+        <div class="card-after">
+        <img src="${cardValues[i].image}" class="image"/></div>
+     </div>
+     `;
   }
-}
-
-//Events fired on the drop target
-function dragOver(e) {
-  e.preventDefault();
-}
-
-//For touchscreen movement
-const touchMove = (e) => {
-  if (moveElement) {
-    e.preventDefault();
-    let newX = e.touches[0].clientX;
-    let newY = e.touches[0].clientY;
-    let currentSelectedElement = document.getElementById(e.target.id);
-    currentSelectedElement.parentElement.style.top =
-      currentSelectedElement.parentElement.offsetTop - (initialY - newY) + "px";
-    currentSelectedElement.parentElement.style.left =
-      currentSelectedElement.parentElement.offsetLeft -
-      (initialX - newX) +
-      "px";
-    initialX = newX;
-    initialY - newY;
-  }
+  //Grid
+  gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
+  //Cards
+  cards = document.querySelectorAll(".card-container");
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      //If selected card is not matched yet then only run (i.e already matched card when clicked would be ignored)
+      if (!card.classList.contains("matched")) {
+        //flip the cliked card
+        card.classList.add("flipped");
+        //if it is the firstcard (!firstCard since firstCard is initially false)
+        if (!firstCard) {
+          //so current card will become firstCard
+          firstCard = card;
+          //current cards value becomes firstCardValue
+          firstCardValue = card.getAttribute("data-card-value");
+        } else {
+          //increment moves since user selected second card
+          movesCounter();
+          //secondCard and value
+          secondCard = card;
+          let secondCardValue = card.getAttribute("data-card-value");
+          if (firstCardValue == secondCardValue) {
+            //if both cards match add matched class so these cards would beignored next time
+            firstCard.classList.add("matched");
+            secondCard.classList.add("matched");
+            //set firstCard to false since next card would be first now
+            firstCard = false;
+            //winCount increment as user found a correct match
+            winCount += 1;
+            //check if winCount ==half of cardValues
+            if (winCount == Math.floor(cardValues.length / 2)) {
+              result.innerHTML = `<h2>You Won</h2>
+            <h4>Moves: ${movesCount}</h4>`;
+              stopGame();
+            }
+          } else {
+            //if the cards dont match
+            //flip the cards back to normal
+            let [tempFirst, tempSecond] = [firstCard, secondCard];
+            firstCard = false;
+            secondCard = false;
+            let delay = setTimeout(() => {
+              tempFirst.classList.remove("flipped");
+              tempSecond.classList.remove("flipped");
+            }, 900);
+          }
+        }
+      }
+    });
+  });
 };
-
-const drop = (e) => {
-  e.preventDefault();
-  //For touch screen
-  if (isTouchDevice()) {
-    moveElement = false;
-    //Select country name div using the custom attribute
-    const currentDrop = document.querySelector(`div[data-id='${e.target.id}']`);
-    //Get boundaries of div
-    const currentDropBound = currentDrop.getBoundingClientRect();
-    //if the position of flag falls inside the bounds of the countru name
-    if (
-      initialX >= currentDropBound.left &&
-      initialX <= currentDropBound.right &&
-      initialY >= currentDropBound.top &&
-      initialY <= currentDropBound.bottom
-    ) {
-      currentDrop.classList.add("dropped");
-      //hide actual image
-      currentElement.classList.add("hide");
-      currentDrop.innerHTML = ``;
-      //Insert new img element
-      currentDrop.insertAdjacentHTML(
-        "afterbegin",
-        `<img src= "${currentElement.id}.png">`
-      );
-      count += 1;
-    }
-  } else {
-    //Access data
-    const draggedElementData = e.dataTransfer.getData("text");
-    //Get custom attribute value
-    const droppableElementData = e.target.getAttribute("data-id");
-    if (draggedElementData === droppableElementData) {
-      const draggedElement = document.getElementById(draggedElementData);
-      //dropped class
-      e.target.classList.add("dropped");
-      //hide current img
-      draggedElement.classList.add("hide");
-      //draggable set to false
-      draggedElement.setAttribute("draggable", "false");
-      e.target.innerHTML = ``;
-      //insert new img
-      e.target.insertAdjacentHTML(
-        "afterbegin",
-        `<img src="${draggedElementData}.png">`
-      );
-      count += 1;
-    }
-  }
-  //Win
-  if (count == 3) {
-    result.innerText = `You Won!`;
-    stopGame();
-  }
-};
-
-//Creates flags and countries
-const creator = () => {
-  dragContainer.innerHTML = "";
-  dropContainer.innerHTML = "";
-  let randomData = [];
-  //for string random values in array
-  for (let i = 1; i <= 3; i++) {
-    let randomValue = randomValueGenerator();
-    if (!randomData.includes(randomValue)) {
-      randomData.push(randomValue);
-    } else {
-      //If value already exists then decrement i by 1
-      i -= 1;
-    }
-  }
-  for (let i of randomData) {
-    const flagDiv = document.createElement("div");
-    flagDiv.classList.add("draggable-image");
-    flagDiv.setAttribute("draggable", true);
-    if (isTouchDevice()) {
-      flagDiv.style.position = "absolute";
-    }
-    flagDiv.innerHTML = `<img src="${i}.png" id="${i}">`;
-    dragContainer.appendChild(flagDiv);
-  }
-  //Sort the array randomly before creating country divs
-  randomData = randomData.sort(() => 0.5 - Math.random());
-  for (let i of randomData) {
-    const countryDiv = document.createElement("div");
-    countryDiv.innerHTML = `<div class='countries' data-id='${i}'>
-    ${i.charAt(0).toUpperCase() + i.slice(1).replace("-", " ")}
-    </div>
-    `;
-    dropContainer.appendChild(countryDiv);
-  }
-};
-
-//Start Game
-startButton.addEventListener(
+//Start game
+startButton.addEventListener("click", () => {
+  movesCount = 0;
+  seconds = 0;
+  minutes = 0;
+  //controls amd buttons visibility
+  controls.classList.add("hide");
+  stopButton.classList.remove("hide");
+  startButton.classList.add("hide");
+  //Start timer
+  interval = setInterval(timeGenerator, 1000);
+  //initial moves
+  moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
+  initializer();
+});
+//Stop game
+stopButton.addEventListener(
   "click",
-  (startGame = async () => {
-    currentElement = "";
-    controls.classList.add("hide");
-    startButton.classList.add("hide");
-    //This will wait for creator to create the images and then move forward
-    await creator();
-    count = 0;
-    dropPoints = document.querySelectorAll(".countries");
-    draggableObjects = document.querySelectorAll(".draggable-image");
-
-    //Events
-    draggableObjects.forEach((element) => {
-      element.addEventListener("dragstart", dragStart);
-      //for touch screen
-      element.addEventListener("touchstart", dragStart);
-      element.addEventListener("touchend", drop);
-      element.addEventListener("touchmove", touchMove);
-    });
-    dropPoints.forEach((element) => {
-      element.addEventListener("dragover", dragOver);
-      element.addEventListener("drop", drop);
-    });
+  (stopGame = () => {
+    controls.classList.remove("hide");
+    stopButton.classList.add("hide");
+    startButton.classList.remove("hide");
+    clearInterval(interval);
   })
 );
+//Initialize values and func calls
+const initializer = () => {
+  result.innerText = "";
+  winCount = 0;
+  let cardValues = generateRandom();
+  console.log(cardValues);
+  matrixGenerator(cardValues);
+};
